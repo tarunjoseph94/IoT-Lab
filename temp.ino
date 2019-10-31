0c79e69183ac3d56865a41bbe8abf1976750e1cb
Tarun Joseph <tarunjoseph94@gmail.com>
2019-10-31 10:44:06 +0530

    Temp sensor
    Used a DHT11 code for the rest is there based on the comments

---
+++ temp.ino
@@ -0,0 +1,48 @@
+/*Install the libraries bfore starting 
+ * ||tools->Manage libraries
+                     |->Adafruit unifed Sensor
+                     |->DHT Sensor Library */ 
+
+#include "DHT.h"
+
+#define DHTPIN 4     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHTxx test!");
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print("\n ");
+
+}
